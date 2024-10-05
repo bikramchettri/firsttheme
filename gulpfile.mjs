5111441912edb07b29dea503714f8ea6e4aec417
Bikram Chettri <bikramchettri120@outlook.com>
2024-10-05 10:09:30 +0530

    Gulp Imagemin issue solved

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -4,18 +4,45 @@ import { hideBin } from "yargs/helpers";
 import * as dartSass from "sass";
 import gulpSass from "gulp-sass";
 import cleanCSS from "gulp-clean-css";
-import gulpIf from "gulp-if";
+import gulpif from "gulp-if";
+import sourcemaps from "gulp-sourcemaps";
+import imagemin from "gulp-imagemin";
 
 const PRODUCTION = yargs(hideBin(process.argv)).argv.prod;
 
 const sass = gulpSass(dartSass);
 
-export const styles = () => {
-  return gulp
-    .src("src/assets/scss/bundle.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulpIf(PRODUCTION, cleanCSS({ compatibility: "ie8" })))
-    .pipe(gulp.dest("dist/assets/css/"));
+const paths = {
+  styles: {
+    src: ["src/assets/scss/bundle.scss", "src/assets/scss/admin.scss"],
+    dest: "dist/assets/css",
+  },
+  images: {
+    src: "src/assets/images/**/*.{jpg,jpeg,png,svg,gif}",
+    dest: "dist/assets/images",
+  },
 };
 
-// export default hello;
+export const styles = () => {
+  return gulp
+    .src(paths.styles.src)
+    .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulpif(PRODUCTION, cleanCSS({ compatibility: "ie8" })))
+    .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+    .pipe(gulp.dest(paths.styles.dest));
+};
+
+export const images = () => {
+  return (
+    gulp
+      // gulp imagemin breaking images and not optimizing, solve by , { encoding: false }
+      .src(paths.images.src, { encoding: false })
+      .pipe(gulpif(PRODUCTION, imagemin({ verbose: true })))
+      .pipe(gulp.dest(paths.images.dest))
+  );
+};
+
+export const watch = () => {
+  gulp.watch("src/assets/scss/**/**.scss", styles);
+};
